e0f8bf20895c5b278654e912942fa19c27a3907e
Alex Matos <alexs.matos@hotmail.com>
2023-03-10 22:20:43 -0300

    add modal create

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -2,7 +2,7 @@
     <h2 v-if="mudar=='squad'">Lista de Squads</h2>      
     <h2 v-if="mudar=='user'">Lista de Usuários</h2>      
 
-    <div v-if="mudar=='squad'" class="listInfo">
+    <div v-if="mudar=='squad' && !squadHours" class="listInfo">
         <div v-if="!haveSquad" class="">
             <img src="img/Emoji.png" alt="">      
             <p class="text-info">Nenhuma squad cadastrada. Crie uma squad para começar.</p>   
@@ -17,16 +17,16 @@
                 <tr class="tableData" v-for="(squad, index) in allSquad" :key="squad.id">
                     <td>{{ squad.id }}</td>
                     <td>{{ squad.name }}</td>                   
-                    <td><button class="btn-table">Visitar Squad</button></td>
+                    <td><button class="btn-table" @click="squadSelected(squad.id)">Visitar Squad</button></td>
                 </tr>
             </table>
         </div>
         <div>
-            <button class="btn-create">Criar Squad</button>
+            <button class="btn-create" @click="squadShow()">Criar Squad</button>
         </div>
     </div>
 
-    <div v-if="mudar == 'user'" class="listInfo">
+    <div v-if="mudar == 'user' && !squadHours" class="listInfo">
         <div v-if="!haveEmployee">
             <img src="img/Emoji.png" alt="">      
             <p class="text-info">Nenhuma Usuário cadastrado. Crie um Usuário para começar.</p>   
@@ -42,20 +42,92 @@
                     <td class="user-table-left">{{ employee.name }}</td>
                     <td>{{ employee.estimatedHours }}</td>
                     <td>{{ employee.squadId }}</td>
-                    <!-- <td><button class="btn-table">Visitar Squad</button></td> -->
                 </tr>
             </table>
         </div>
         <div>
-            <button class="btn-create">Criar Usuário</button>
+            <button class="btn-create2" @click="userShow()">Criar Usuário</button>
         </div>
+    </div>
+
+    <div v-if="squadHours" class="listSquad">
+        <div v-if="!haveEmployee">
+            <img src="img/Emoji.png" alt="">      
+            <p class="text-info">Nenhuma Usuário cadastrado. Crie um Usuário para começar.</p>   
+        </div>
+        <div v-else>            
+            <input type="date" name="" id="" v-model="dataInicial">
+            <input type="date" name="" id="" v-model="dataFinal">
+            <button class="btn-filtro" @click="listSquadTime(squadId)">Filtrar por data</button>
+
+            <p class="title-squad-list">Horas por membro</p>
+
+            <table class="table" cellspacing="0">
+                <tr class="titulo">
+                    <th class="list-table-left">Membro</th>
+                    <th class="list-table-middle">Descrição</th>
+                    <th>Horas</th>
+                    <th class="list-table-right">Criado em</th>
+                </tr>
+                <tr class="tableData" v-for="(stime, index) in squadTime" :key="index">
+                    <td class="list-table-left">{{ stime.name }}</td>
+                    <td>{{ stime.description }}</td>
+                    <td>{{ stime.spentHours }}</td>
+                    <td>{{ stime.createdAt }}</td>
+                </tr>
+            </table>
+
+            <div v-if="quantidade > 0">
+                <p class="title-squad-list">Horas totais da Squad</p>
+                <p class="text-squad-list">{{ squadTotal.squadHours<=1 ? squadTotal.squadHours+" Hora": squadTotal.squadHours+" Horas"}}</p>
+            </div>
+            <div v-if="quantidade > 0">
+                <p class="title-squad-list">Média de horas por dia</p>
+                <p class="text-squad-list">{{ (squadTotal.squadHours/quantidade)<=1 ? (squadTotal.squadHours/quantidade)+" Hora/Dia": (squadTotal.squadHours/quantidade)+" Horas/Dia"}}</p>
+            </div>
+        </div>
+        <div>
+            <button class="btn-create" @click="userShow()">Adicionar Usuário</button>
+        </div>
+    </div>
+
+    <div v-if="showCreateSquad" class="modal-create-squad">
+        <p class="title-squad-create">Criar Squad</p>
+        <div class="div-imput-squad">
+            <label class="label-squad" for="cadastro">nome da squad</label>
+            <br>
+            <input type="text" name="cadastro" v-model="nomeSquad" placeholder="Digite o nome da squad">
+        </div>
+        
+        <button class="btn-create" @click="createSquad()">Criar squad</button>
+    </div>
+
+
+    <div v-if="showCreateUser" class="modal-create-user">
+        <p class="title-squad-create">Criar Usuário</p>
+        <div class="div-imput-squad">
+            <label class="label-squad" for="cadastro">Nome do usuário</label>
+            <br>
+            <input type="text" name="cadastro" v-model="nomeUsuario" placeholder="Digite o nome do usuário">
+        </div>
+        <div class="div-imput-squad">
+            <label class="label-squad" for="cadastro">Horas estimadas de trabalho</label>
+            <br>
+            <input type="number" name="cadastro" v-model="horasEstimadas" placeholder="Digite a quantidade de Horas">
+        </div>
+        <div class="div-imput-squad">
+            <label class="label-squad" for="cadastro">Squad</label>
+            <br>
+            <input type="number" name="cadastro" v-model="idSquad" placeholder="Digite o ID da squad">
+        </div>        
+        
+        <button class="btn-create" @click="createUser()">Criar usuário</button>
     </div>
 
 </template>
 
 <script>
 import axios from 'axios';
-import { stringifyExpression } from '@vue/compiler-core';
 
     export default{
         name: 'Header',
@@ -66,9 +138,22 @@ import { stringifyExpression } from '@vue/compiler-core';
             return{
                allSquad:[],
                allEmployee:[],
+               squadTime:[],
                haveSquad: false,
                haveEmployee: false,
                tituloAba: "",
+               squadHours: false,
+               squadId: 0,
+               dataInicial:"",
+               dataFinal:"",
+               quantidade: 0,
+               squadTotal:{},
+               nomeSquad: "",
+               showCreateSquad: false,
+               showCreateUser: false,
+               nomeUsuario: "",
+               horasEstimadas: 0,
+               idSquad: 0
 
             }
         },
@@ -93,7 +178,76 @@ import { stringifyExpression } from '@vue/compiler-core';
                     this.tituloAba = "Lista de Usuários"
                 }
 
+            },
+
+            squadSelected(id){
+                this.squadId = id
+                this.squadHours = true;
+                this.showCreateSquad = false
+                this.showCreateUser = false
+            },
+
+            async listSquadTime(id){
+                this.quantidade = 0;
+                let dataInicio = this.dataInicial;
+                let dataFim = this.dataFinal;
+
+                let results = await axios.post(`/reportList/${id}/${dataInicio}/${dataFim}`);
+                this.squadTime = results.data;
+
+                this.quantidade = results.data.length
+
+                this.squadTotalTime(id)
+            },
+
+            async squadTotalTime(id){
+          
+                let results = await axios.post(`/squadTime/${id}`);
+                this.squadTotal = results.data[0];                
+            },
+
+            async createSquad(){
+                let nome = this.nomeSquad
+                this.showCreateUser = false
+                let result = await axios.post(`/squad`, {name: nome}); 
+              
+
+                if(result.status == 200){
+                    this.nomeSquad = ""
+                    this.showCreateSquad = false
+
+                    alert("Squad criado com sucesso")
+                }
+                
+            },
+
+            async createUser(){
+                let nome = this.nomeUsuario
+               let horas = this.horasEstimadas
+               let squadId = this.idSquad
+               this.showCreateSquad = false
+
+               let result = await axios.post(`/employee`, {name: nome, estimatedHours: horas, squadId: squadId});   
+               
+               if(result.status == 200){
+                this.nomeUsuario = ""
+                this.horasEstimadas = 0
+                this.idSquad = 0
+                this.showCreateUser = false
+                    alert("Usuário criado com sucesso")
+                }
+            },
+
+            squadShow(){
+                this.showCreateSquad = !this.showCreateSquad
+                this.showCreateUser = false
+            },
+
+            userShow(){
+                this.showCreateUser = !this.showCreateUser
+                this.showCreateSquad = false
             }
+           
         },     
         mounted() {
             this.getSquads();
@@ -109,6 +263,17 @@ import { stringifyExpression } from '@vue/compiler-core';
 .listInfo{
     height: 411px;
     width: 490px;
+    left: 168px;
+    top: 355px;
+    border-radius: 12px;
+    padding: 64px 32px 64px 32px;
+    background-color: #FFF;
+    text-align: center;
+}
+
+.listSquad{
+    height: 788px;
+    width: 100%;
     left: 168px;
     top: 355px;
     border-radius: 12px;
@@ -135,6 +300,21 @@ import { stringifyExpression } from '@vue/compiler-core';
 }
 
 .btn-create:hover{
+    background-color: #5d7bf5;
+}
+
+.btn-create2{
+    width: 182px;
+    height: 48px;
+    background-color: #4263EB;
+    color: #FFF;
+    border-radius: 8px;
+    border: none;
+    margin-top: 20px;
+    cursor: pointer;
+}
+
+.btn-create2:hover{
     background-color: #5d7bf5;
 }
 
@@ -203,10 +383,121 @@ tr td{
     width: 20%;
 }
 
+.list-table-left{
+    width: 30%;
+    border-top-left-radius: 5px !important
+}
+
+.list-table-middle{
+    width: 40%;
+}
+
+.list-table-right{
+    width: 15%;
+    border-top-right-radius: 5px !important
+}
 
 h2{
     font-weight: 500;
     font-size: 38px;
     margin-bottom: 30px;
 }
+
+.btn-filtro{
+    width: 182px;
+    height: 40px;
+    background-color: #4263EB;
+    color: #FFF;
+    border-radius: 8px;
+    border: none;
+    margin-top: 70px;
+    cursor: pointer;    
+}
+
+.btn-filtro:hover{
+    background-color: #5d7bf5;
+}
+
+input[type="date"] { 
+  background: transparent;  
+  height: 40px;
+  border-radius: 8px;
+  border: 1px solid #DDE2E5;
+  margin-right: 10px;
+}
+
+input[type="text"] { 
+    width: 350px;
+    height: 56px;
+    font-size: 20px;
+    border-radius: 8px;
+    border: 1px solid #DDE2E5;
+}
+
+input[type="number"] { 
+    width: 350px;
+    height: 56px;
+    font-size: 20px;
+    border-radius: 8px;
+    border: 1px solid #DDE2E5;
+}
+
+.title-squad-list{
+    font-size: 28px;
+    font-weight: 400;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.title-squad-create{
+    font-size: 38px;
+    font-weight: 500;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-bottom: 55px;
+}
+
+.text-squad-list{
+    color: #4263EB;
+    font-size: 50px;
+}
+
+::placeholder {
+  font-size: 21px;
+  color: #DDE2E5;
+  padding-left: 20px;
+}
+
+.modal-create-squad{
+    width: 414px;
+    height: 396px;
+    position: absolute;
+    background-color: #FFF;
+    text-align: center;
+    margin-left: 540px;
+    margin-top: -425px;
+}
+
+.modal-create-user{
+    width: 414px;
+    height: 618px;
+    position: absolute;
+    background-color: #FFF;
+    text-align: center;
+    margin-left: 540px;
+    margin-top: -425px;
+}
+
+.div-imput-squad{
+    text-align: left; 
+    margin-left: 7%;
+}
+
+.label-squad{
+    color: #ACB5BD; 
+    font-size: 12px; 
+    text-transform: uppercase; 
+    font-weight: 500;
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,28 +6,80 @@
     </div>
     <div>
         <h1 class="mainTitle">{{ titulo }}</h1>
-        <button class="btn-ajust">Lançar horas</button>
+        <button class="btn-ajust" @click="showReport()">Lançar horas</button>
     </div>
     <div class="bottom-header-btn">
         <button class="btn-aba" @click="$emit('change', {aba: 'squad'})">Squads</button>
         <button class="btn-aba" @click="$emit('change', {aba: 'user'})">Usuários</button>
     </div>
+
+    <div v-if="showCreateReport" class="modal-create-report"> 
+        <p class="title-report-create">Criar Lançamento</p>
+        
+        <div class="div-imput-report">
+            <label class="label-report" for="cadastro">id do usuário</label>
+            <br>
+            <input type="number" name="cadastro" v-model="usuarioId" placeholder="Digite o ID do usuário">
+        </div>
+        <div class="div-imput-report">
+            <label class="label-report" for="cadastro">Horas Gastas</label>
+            <br>
+            <input type="number" name="cadastro" v-model="horasGastas" placeholder="Digite a quantidade de Horas">
+        </div>
+        <div class="div-imput-report">
+            <label class="label-report" for="cadastro">descrição</label>
+            <br>
+            <input type="text" name="cadastro" v-model="descricao" placeholder="Adicione uma descrição">
+        </div>        
+        
+        <button class="btn-create" @click="createReport()">Criar lançamento</button>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
     export default{
         name: 'Header',
         emits:['change'],
         data(){
             return{
-               titulo: "PD Hours"
+               titulo: "PD Hours",
+               showCreateReport: false,
+               usuarioId: 0,
+               horasGastas: 0,
+               descricao: ""
             }
+        },
+        methods:{
+            showReport(){
+                this.showCreateReport = !this.showCreateReport
+            },
+
+            async createReport(){
+                let descricao = this.usuarioId
+               let userId = this.horasGastas
+               let horas = this.descricao
+
+               let result = await axios.post(`/report`, {description: descricao, employeeId: userId, spentHours: horas});   
+               
+               if(result.status == 200){
+                    this.usuarioId = 0
+                    this.horasGastas = 0
+                    this.descricao = ""
+                    this.showCreateReport = false
+
+                    alert("Lançamento criado com sucesso")
+                }
+                else{
+                    alert("Erro ao criar Lançamento")
+                }
+            },
         }
     }
 </script>
 
 
-<style>
+<style scoped>
 
 .img-ajust{
     margin-top: 14px;
@@ -89,4 +141,70 @@
     border-bottom: 5px solid #4263EB;
     cursor: pointer;
 }
+
+
+input[type="text"] { 
+    width: 350px;
+    height: 100px;
+    font-size: 20px;
+    border-radius: 8px;
+    border: 1px solid #DDE2E5;
+}
+
+input[type="number"] { 
+    width: 350px;
+    height: 56px;
+    font-size: 20px;
+    border-radius: 8px;
+    border: 1px solid #DDE2E5;
+}
+
+.title-report-list{
+    font-size: 28px;
+    font-weight: 400;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.title-report-create{
+    font-size: 38px;
+    font-weight: 500;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-bottom: 55px;
+}
+
+.text-report-list{
+    color: #4263EB;
+    font-size: 50px;
+}
+
+::placeholder {
+  font-size: 21px;
+  color: #DDE2E5;
+  padding-left: 20px;
+}
+
+.modal-create-report{
+    width: 414px;
+    height: 396px;
+    position: absolute;
+    background-color: #FFF;
+    text-align: center;
+    margin-left: 540px;
+    margin-top: 0px;
+}
+
+.div-imput-report{
+    text-align: left; 
+    margin-left: 7%;
+}
+
+.label-report{
+    color: #ACB5BD; 
+    font-size: 12px; 
+    text-transform: uppercase; 
+    font-weight: 500;
+}
+
 </style>
